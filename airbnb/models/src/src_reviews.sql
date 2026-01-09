c445019ca2ed44ed35a85246d6de3301a8d6997c
mv-matt <matthieu.vincenten@metaverses.eu>
2026-01-09 17:07:27 +0100

    2026-01-09_03

--- airbnb/models/src/src_reviews.sql
+++ airbnb/models/src/src_reviews.sql
@@ -1,12 +1,14 @@
 WITH raw_reviews AS (
-    SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
+    SELECT
+        *
+    FROM
+        {{ source('airbnb', 'reviews') }}
 )
 SELECT
     listing_id,
-    date AS review_date,
+    DATE AS review_date,
     reviewer_name,
     comments AS review_text,
     sentiment AS review_sentiment
 FROM
     raw_reviews
-
